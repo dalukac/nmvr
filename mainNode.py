3770184816f84fc2b0c1cc32b7c8df20d8037493
vetr0lam <dlukac44@gmail.com>
2021-10-20 22:39:48 +0200

    main node edit

--- mainNode.py
+++ mainNode.py
@@ -3,9 +3,9 @@ import os
 def main():
     print('Main Node initialized')
     mydir = os.getcwd() 
-    os.system("gnome-terminal -e 'bash -c \"python3 " + mydir + "subscriber.py; bash\"'")
-    os.system("gnome-terminal -e 'bash -c \"python3 " + mydir + "publisher.py; bash\"'")
-    os.system("gnome-terminal -e 'bash -c \"python3 " + mydir + "map_plot.py; bash\"'")
+    os.system("terminal -e 'bash -c \"python3 " + mydir + "subscriber.py; bash\"'")
+    os.system("terminal -e 'bash -c \"python3 " + mydir + "publisher.py; bash\"'")
+    os.system("terminal -e 'bash -c \"python3 " + mydir + "map_plot.py; bash\"'")
     print(mydir)
 
 if __name__ == '__main__':
